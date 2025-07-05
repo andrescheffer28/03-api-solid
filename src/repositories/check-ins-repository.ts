e9fb06621616d1e1d9e3ae7231fd694f3b5da3a8
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-05 19:19:26 -0300

    Caso de uso de métricas

--- src/repositories/check-ins-repository.ts
+++ src/repositories/check-ins-repository.ts
@@ -3,6 +3,7 @@ import { CheckIn, Prisma } from "@prisma/client";
 export interface CheckInsRepository{
     create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
     findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+    countByUserId(userId: string): Promise<number>
     // procura se existe um check-in de um usuário em uma determinada data
     findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null>
 }
