fbe9cf22e2c1d233c0f2a5ca1e6838dd0d6bbda8
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-06 14:45:26 -0300

    Controllers & Testes E2E - Implementando JWT no Fastify

--- src/env/index.ts
+++ src/env/index.ts
@@ -6,6 +6,7 @@ import { z } from 'zod'
 
 const envSchema = z.object({
     NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    JWT_SECRET: z.string(),
     PORT: z.coerce.number().default(3333), //coerce realiza a conversão de um tipo ex, string, para o tipo pretendindo ex, number
 
 })
