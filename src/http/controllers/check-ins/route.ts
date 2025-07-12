d8766065cf458e30504fb9929b989c9364f8f732
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-12 10:50:04 -0300

    Controllers & Testes E2E - Testes E2E de rotas de academias

--- src/http/controllers/check-ins/route.ts
+++ src/http/controllers/check-ins/route.ts
@@ -11,7 +11,7 @@ export async function checkInsRoutes(app: FastifyInstance) {
 
     //como o chekin vai sempre precisar do id, pode ser passado pela rota
     app.get('/check-ins/history', history)
-    app.get('/check-ins/history', metrics)
+    app.get('/check-ins/metrics', metrics)
 
     app.post('/gym/:gymId/check-ins', create)
     app.patch('/check-ins/:checkInId/validate', validate)
