5ba6b227fa521aa4fab04ddf76a32d823cc403d0
andreScheffer <andre.scheffer@ctjunior.com.br>
2025-07-12 11:21:44 -0300

    erro404 able to check in

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,38 @@
+import request from 'supertest'
+import { app } from 'src/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { createAndAuthenticateUser } from 'src/utils/create-and-authenticate-use'
+import { prisma } from 'src/lib/prisma'
+
+describe('Create Check-in (e2e)', () => {
+
+    beforeAll(async () => {
+        await app.ready()
+    })
+    afterAll(async () => {
+        await app.close()
+    })
+
+
+    it('should be able to create a check-in', async () => {
+        const { token } = await createAndAuthenticateUser(app)
+
+        const gym = await prisma.gym.create({
+            data:{
+                title: 'JavaScript Gym',
+                latitude: 0,
+                longitude: 0,
+            }
+        })
+
+        const response = await request(app.server)
+            .post(`/gyms/${gym.id}/check-ins`)
+            .set('Authorization', `Bearer ${token}`)
+            .send({
+                latitude: 0,
+                longitude: 0,
+            })
+
+        expect(response.statusCode).toEqual(201)
+    })
+})
